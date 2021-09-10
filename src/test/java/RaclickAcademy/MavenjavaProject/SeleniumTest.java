f6649c5d8571f77cc9380b9357989ba215598eb0
hp <sharanpadashetty637@gmail.com>
2021-09-10 22:02:16 +0530

    American guy commit

--- src/test/java/RaclickAcademy/MavenjavaProject/SeleniumTest.java
+++ src/test/java/RaclickAcademy/MavenjavaProject/SeleniumTest.java
@@ -17,7 +17,10 @@ public class SeleniumTest
 	public void elementsui()
 
 	{
-		System.out.println("elementsui");
+		System.out.println("elementsui1");
+		System.out.println("elementsui2");
+		System.out.println("elementsui3");
+		System.out.println("elementsui4");
 	}
 
 }
