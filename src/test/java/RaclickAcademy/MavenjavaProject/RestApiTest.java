d6a9fb27a42e5b24ff6590d9402c2714b3fff99c
hp <sharanpadashetty637@gmail.com>
2021-09-10 21:52:23 +0530

    this is rvp commit

--- src/test/java/RaclickAcademy/MavenjavaProject/RestApiTest.java
+++ src/test/java/RaclickAcademy/MavenjavaProject/RestApiTest.java
@@ -9,7 +9,12 @@ public class RestApiTest
 	public void PostJira()
 
 	{
-		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
+		System.out.println("PostJira3");
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		
 	}
 
 	@Test
